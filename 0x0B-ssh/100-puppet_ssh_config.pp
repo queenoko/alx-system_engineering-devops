a92a937947d08db3f6e7ef523793aedd47253018
queenoko <quimaudongwo@gmail.com>
2023-09-24 14:20:42 -0700

    confug puppet

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,13 @@
+# This Puppet script creates ssh configuration file
+# using the  private key ~/.ssh/school
+file_line { 'Turn off passwd auth':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    PasswordAuthentication no',
+}
+
+file_line { 'Declare identity file':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    IdentityFile ~/.ssh/school',
+}
